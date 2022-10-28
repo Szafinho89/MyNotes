1b7ba2c70a9f1f4b5086194bde782794091f8b11
Szafinho89 <szafraniak.tomasz@gmail.com>
2022-10-28 16:14:51 +0200

    Completed

--- script.js
+++ script.js
@@ -9,27 +9,30 @@ const notePanel = document.querySelector('.note-panel')
 const category = document.querySelector('#category') //tu dalej pogrzebac
 const textArea = document.querySelector('#text')
 const error = document.querySelector('.error')
-// const noteTitle = document.querySelector('.note-title')
-// const noteBody = document.querySelector('.note-body')
-let selectedValue;
-let cardID = 0;
+
+let selectedValue
+let cardID = 0
 
 const openPanel = () => {
-    notePanel.style.display = 'flex'
+    notePanel.style.display= 'flex'
 }
 
 const closePanel = () => {
-    notePanel.style.display = 'none'
-    error.style.visibility = 'hidden'
+    notePanel.style.display= 'none'
+    category.selectedIndex = 0
     textArea.value = ''
-    category.selectedIndex = 0;  //daje się selectedIndex = 0 a ja zrobilem category.value='0' i tez dzialalo
+    error.style.visibility= 'hidden'
 }
 
 const addNote = () => {
-    if (textArea.value !== '' && category.options[category.selectedIndex].value !== '0') {
+    // console.log(category);
+    error.style.visibility= 'hidden'
+    if (category.selectedIndex !== 0   &&   textArea.value !== '') {
         createNote()
+        closePanel()
     } else {
-        error.style.visibility = 'visible'
+        error.style.visibility= 'visible'
+
     }
 }
 
@@ -37,46 +40,42 @@ const createNote = () => {
     const newNote = document.createElement('div')
     newNote.classList.add('note')
     newNote.setAttribute('id', cardID)
-    newNote.innerHTML= `<div class="note-header"><h3 class="note-title">${selectedValue}</h3><button class="delete-note" onclick="deleteNote(${cardID})"><i class="fas fa-times icon"></i></button></div><div class="note-body">${textArea.value}</div>`
+    newNote.innerHTML= 
+    `<div class="note-header"><h3 class="note-title">${selectedValue}</h3><button class="delete-note" onclick="deleteNote(${cardID})"><i class="fas fa-times icon"></i></button></div><div class="note-body">${textArea.value}</div>`
+    
     noteArea.append(newNote)
-    cardID++
-    closePanel()
     checkColor(newNote)
-
+    cardID++
 }
 
 const selectValue = () => {
     selectedValue = category.options[category.selectedIndex].text
 }
 
-const checkColor = (note) => {
+const checkColor = (newNote) => {
     switch (selectedValue) {
         case 'Zakupy':
-            note.style.backgroundColor= 'yellow'
+            newNote.style.backgroundColor= 'yellow'
             break;
         case 'Praca':
-            note.style.backgroundColor= 'green'
-            break
+            newNote.style.backgroundColor= 'green'
+            break;
         case 'Inne':
-            note.style.backgroundColor= 'blue'
+            newNote.style.backgroundColor= 'blue'
             break;
     }
 }
 
 const deleteNote = (id) => {
-    const noteToDelete = document.getElementById(id);
-    console.log(noteToDelete);
-    noteArea.remove(noteToDelete)
+    const noteToDelete = document.getElementById(id)
+    noteArea.removeChild(noteToDelete)
 }
 
-const deleteAllNotes = () => {
-
-    noteArea.textContent= ''
+const deleteAll = () => {
+    noteArea.innerHTML= ''
 }
 
 addBtn.addEventListener('click', openPanel)
 cancelBtn.addEventListener('click', closePanel)
 saveBtn.addEventListener('click', addNote)
-deleteAllBtn.addEventListener('click', deleteAllNotes)
-
-
+deleteAllBtn.addEventListener('click', deleteAll)
